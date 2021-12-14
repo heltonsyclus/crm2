bbad0b06f307cff7e7dc100fc6dbcf04efe6b0d9
HELTON SYCLUS <84809618+heltonsyclus@users.noreply.github.com>
2021-12-14 17:21:02 -0300

    enviando json

--- src/commands/loginColaborador.js
+++ src/commands/loginColaborador.js
@@ -29,120 +29,217 @@ export function senhaLogin() {
           workflow: { id_layout_workflow: 1 },
           notificacao: { id_layout_notificacao: 1 }
         },
-        token: ""
+        token: "108697"
       },
       {
         usuario: "joaopedro",
         senha: "joaopedroyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "10869227"
       },
       {
         usuario: "abner",
         senha: "abnersyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "10328697"
       },
       {
         usuario: "pedro",
         senha: "pedrosyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "10863297"
       },
       {
         usuario: "maciel",
         senha: "macielsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "10821697"
       },
       {
         usuario: "danilo",
         senha: "0",
         recursos: {
-          area_trabalho: { id_layout_dashboard: 0 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          area_trabalho: { id_layout_dashboard: 1 },
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108333333697"
       },
       {
         usuario: "adelmo",
         senha: "adelmosyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "1086222297"
       },
       {
         usuario: "jonatan",
         senha: "jonatansyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108693217"
       },
       {
         usuario: "helton",
         senha: "heltonsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 },
-          atividade: { id_layout_atividade: 0 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108697"
       },
       {
         usuario: "rodrigo",
         senha: "rodrigosyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108321697"
       },
       {
         usuario: "ewerton",
         senha: "ewertonsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "13108697"
       },
       {
         usuario: "henrique",
         senha: "henriquesyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108697321"
       },
       {
         usuario: "jullyadson",
         senha: "jullyadsonsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "108632197"
       },
       {
         usuario: "eron",
         senha: "eronsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "103218697"
       },
       {
         usuario: "clederson",
         senha: "cledersonsyclus",
         recursos: {
           area_trabalho: { id_layout_dashboard: 1 },
-          cliente: { id_layout_dashboard: 5 }
-        }
+          atividade: { id_layout_atividade: 1 },
+          projetos: { id_layout_projetos: 1 },
+          cliente: { id_layout_cliente: 1 },
+          ocorrencia: { id_layout_ocorrencia: 1 },
+          agenda: { id_layout_agenda: 1 },
+          workflow: { id_layout_workflow: 1 },
+          notificacao: { id_layout_notificacao: 1 }
+        },
+        token: "1321308697"
       }
     ]
   };
